8fd7c0ea7d45a52ce37a351ad8fa63fe030ec123
Soumil Roy <hello@soumilroy.com>
2022-04-01 04:31:19 +0530

    config

---
+++ eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "airbnb-base",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["prettier", "@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": 0,
+    "prettier/prettier": "error",
+    "no-unused-expressions": 0,
+    "import/no-unresolved": 0,
+    "import/extensions": 0,
+    "prefer-default-export": 0,
+    "@typescript-eslint/no-non-null-assertion": 0,
+    "default-case": 0,
+    "import/prefer-default-export": 0,
+    "no-lonely-if": 0,
+  },
+};
